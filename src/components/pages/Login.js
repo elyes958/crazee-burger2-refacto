6119cae5ad32dcdbd03bc43a0176ce42d5261328
elyes95 <elyes.aitsaid@gmail.com>
2024-09-29 19:26:53 +0200

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -2,29 +2,30 @@ import { useState } from "react";
 
 export default function Login() {
     // state
-    const [prenom, setPrenom] = useState("");
+    const [inputValue, setInputValue] = useState("");
   
     // comportement
     const handleSubmit = (event) => {
-      event.preventDefault(); // il faut recharger la page pour vider le formulaire et donc l'input(non en faite je me suis tromper il faut synchroniser l'input avec la valeur du state(prenom) pour faire cela !)
+      event.preventDefault(); // il faut recharger la page pour vider le formulaire et donc l'input(non en faite je me suis tromper il faut synchroniser l'input avec la valeur du state(inputValue) pour faire cela !)
       // console.log(event);
-      alert("Bonjour " + prenom);
-      setPrenom("");
+      alert("Bonjour " + inputValue);
+      setInputValue("");
     }
-
+        
     const handleChange = (event) => {
       const userName =  event.target.value;
-      console.log(userName);
-      setPrenom(userName);
+      // console.log(userName);
+      setInputValue(userName);
     }
   
     // affichage
     return (
       <div>
           <h1>Bienvenue chez nous !</h1>
-          <h3>Connectez-vous</h3>
-          <form onSubmit={handleSubmit}>
-              <input onChange={handleChange} value={prenom} type="text" placeholder="Entrer votre prénom..." required/>
+          <br />
+          <h2>Connectez-vous</h2>
+          <form action="submit" onSubmit={handleSubmit}>
+              <input onChange={handleChange} value={inputValue} type="text" placeholder="Entrer votre prénom..." required/>
               <button>Accéder à votre espace</button>
           </form>
       </div>
