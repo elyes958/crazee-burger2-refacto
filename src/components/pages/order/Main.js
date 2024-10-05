6568a2fc16142b6f7b79dbd2a94b27c9da8e6a47
elyes95 <elyes.aitsaid@gmail.com>
2024-10-05 15:49:35 +0200

    💄 F05 > ORDER > Main > Add grid css

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -3,7 +3,13 @@ import { theme } from "../../../theme";
 
 export default function Main() {
   return (
-    <MainStyled></MainStyled>
+    <MainStyled>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </MainStyled>
   )
 }
 
@@ -17,4 +23,16 @@ const MainStyled = styled.main`
     border-bottom-left-radius: 15px;
     background: #F5F5F7;
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+    box-sizing: border-box;
+    padding: 50px 92px;
+
+    display: grid;     // initialiser grid
+    grid-template-columns:repeat(4, 240px);
+    grid-template-rows: repeat(4, 330px);
+    grid-row-gap: 60px;
+    grid-column-gap: 85px;
+
+    div{
+      border: 1px solid blue;
+    }
 `;
