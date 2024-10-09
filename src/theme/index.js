38510b7eb528b53512f36a7649d0097eaab5f878
elyes95 <elyes.aitsaid@gmail.com>
2024-10-09 13:20:49 +0200

    ♻️ F07 > THEME > update index.js

--- src/theme/index.js
+++ src/theme/index.js
@@ -33,7 +33,8 @@ const fonts = {
   XXXS: "8px",
   XXS: "10px",
   XS: "12px",
-  P0: "15px",
+  SM: "15px",
+  P0: "16px",
   P1: "18px",
   P2: "20px",
   P3: "24px",
@@ -41,14 +42,7 @@ const fonts = {
   P5: "48px",
   P6: "60px",
 }
-const gridUnit = 8
-const borderRadius = {
-  subtle: 1,
-  round: "5px",
-  extraRound: "15px",
-  circle: "50%",
-}
-const shadows = {}
+
 const weights = {
   light: 300,
   regular: 400,
@@ -57,6 +51,22 @@ const weights = {
   bold: 700,
   heavy: 800,
 }
+
+const gridUnit = 8
+
+const borderRadius = {
+  subtle: 1,
+  round: "5px",
+  extraRound: "15px",
+  circle: "50%",
+}
+
+const shadows = {
+  subtle: "0px -6px 8px -2px rgba(0, 0, 0, 0.1)",
+  medium: "-8px 8px 20px 0px rgb(0 0 0 / 20%)",
+  strong: "0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset",
+}
+
 
 export const theme = {
   colors,
