0ad74c773d0bcbd6ce0f7433e948cd70e5d9ba0f
elyes95 <elyes.aitsaid@gmail.com>
2024-10-24 18:30:38 +0200

    🚧 F10: Main > Basket add css

--- src/components/pages/order/Main/Basket.js
+++ src/components/pages/order/Main/Basket.js
@@ -5,11 +5,11 @@ export default function Basket() {
     <BasketStyled>
         <div className="total-price">
             <div>
-                <span>TOTAL</span>
-                <span>0,00 €</span>
+                <span className="total">TOTAL</span>
+                <span className="price">0,00 €</span>
             </div>
         </div>
-        <span>VOTRE COMMANDE EST VIDE.</span>
+        <span className="basket-empty">VOTRE COMMANDE EST VIDE.</span>
         <div className="footer">
             <span>Codé avec ❤️ et React.JS</span>
         </div>
@@ -18,13 +18,62 @@ export default function Basket() {
 }
 
 const BasketStyled = styled.div`
-  border: 1px solid blue;
+  /* border: 1px solid blue; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: white;
 
   .total-price{
-    border: 1px solid red;
+    /* border: 1px solid red; */
+    width: 100%;
+    height: 70px;
+    padding: 0px 16px 0px 16px;
+    background: #292729;
+
+    div{
+       display: flex;
+       justify-content: space-between;
+       padding: 12px 2px 13px 0px;
+
+       .total{
+         font-family: "Amatic SC";
+         font-weight: 400;
+         font-size: 36px;
+         line-height: 45px;
+         color: #FFA01B;
+       }
+       .price{
+         font-family: "Amatic SC";
+         font-weight: 700;
+         font-size: 36px;
+         line-height: 45px;
+         color: #FFA01B;
+       }
+    }
+  }
+
+  .basket-empty{
+    font-family: "Amatic SC";
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: center;
+    color: #747B91;
   }
 
   .footer{
-    border: 1px solid green;
+    /* border: 1px solid green; */
+    background: #292729;
+    border-bottom-left-radius: 15px;
+    padding: 22px 75px 22px 75px;
+
+    span{
+        font-family: "Amatic SC", cursive;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 25px;
+        color: white;
+    }
   }
 `;
